f3098e93d2d1b545d5988812a975a0dcccffcb9c
Shafiq Ahmed <ahmedshaf02@gmail.com>
2020-06-13 00:29:59 +0530

    landing page complete

--- src/Component/LandingPage.jsx
+++ src/Component/LandingPage.jsx
@@ -1,25 +1,16 @@
 
 import React from "react";
-// import NavigationLanding from "./NavigationLanding"
+import "./landing.css"
+import NavigationLanding from "./NavigationLanding"
 
-const Styles = {
-
-cycleMoving:{
-  // position:"absolute",
-  transform:"rotate(-45deg)",
-  // width:300,
-
-
-}
-}
 const LandingPage = ()=>{
 return(
 
   <>
-    <div style={Styles.pageBackground}>
-        {/* <NavigationLanding/> */}
+    <div>
+        <NavigationLanding/>
         <div>
-          <img className="img-fluid" style={Styles.cycleMoving} src="https://i.ya-webdesign.com/images/bicycle-png-icon-4.png" alt="cycle"/>
+          <img className="img-fluid cycleMoving"  src="https://i.ya-webdesign.com/images/bicycle-png-icon-4.png" alt="cycle"/>
         </div>
 
     </div>
---
+++ src/Component/NavigationLanding.jsx
@@ -0,0 +1,17 @@
+
+
+import React from "react";
+import "./landing.css"
+
+const NavigationLanding = ()=>{
+return(
+
+  <>
+    <div>
+       
+    </div>
+  </>
+)
+}
+
+export default NavigationLanding
